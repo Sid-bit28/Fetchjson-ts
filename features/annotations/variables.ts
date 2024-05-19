0024e60f8bf2de7e271b8d2152c459fe21e644c6
Siddarth Banerjee <siddarth2801@gmail.com>
2024-05-19 22:42:51 +0530

    other annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -29,3 +29,31 @@ const logNumber: (i: number) => void = (i: number) => {
 };
 
 //If declaration and initialization is on the same line then typescript will predict what is the type of the variable.
+
+//when to use annotations
+// 1) Function that returns the 'any' type
+
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable in one line and initialize it later in the code.
+
+let words = ['red', 'green'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] == 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) A variable whose type cannot be inferred correctly.
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
